f0cbac353003f3475196a4b46cd473f8f3b389d5
MarcusKri <marcus-kristoffersen@outlook.com>
2022-03-22 20:36:32 +0100

    Server react from express

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
-const server = app.listen(process.env.PORT || 3000, () =>{
-    console.log(`http://localhost:${server.address().port}`)
+app.use(express.static("../client/dist"));
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`http://localhost:${server.address().port}`);
 });
